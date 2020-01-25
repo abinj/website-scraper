79ec7d7cb82910af0cbc4974279279abed282684
abinj <abinmj656@gmail.com>
2020-01-25 16:16:59 -0500

    Head and body part scraping done

--- web-scrapy.py
+++ web-scrapy.py
@@ -1,11 +1,10 @@
-from selenium import webdriver
-from BeautifulSoup import BeautifulSoup
-import pandas as pd
+from urllib2 import urlopen
+from bs4 import BeautifulSoup
 
 
-driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
 
-products=[] #List to store name of the product
-prices=[] #List to store price of the product
-ratings=[] #List to store rating of the product
-driver.get("<a href="https://www.flipkart.com/laptops/">https://www.flipkart.com/laptops/</a>~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&uniq")
+url = "https://www.mathematicalmichael.com/"
+html = urlopen(url)
+soup = BeautifulSoup(html, 'lxml')
+text = soup.find_all('body')
+print(text)
